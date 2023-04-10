565217093093cda1be34b0fd8d1d3b48b46c8710
Kanchan <kanchan08642@gmail.com>
2023-04-10 21:11:52 +0530

    updated

--- my-app/src/components/Portfolio.jsx
+++ my-app/src/components/Portfolio.jsx
@@ -82,7 +82,7 @@ const Portfolio = () => {
       name="portfolio"
       className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen"
     >
-      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+      <div className="max-w-screen-lg p-2 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
             Portfolio
@@ -90,7 +90,7 @@ const Portfolio = () => {
           <p className="py-6">Check out some of my work right here</p>
         </div>
 
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
+        <div className="gap-8 px-12 sm:px-0">
         <Slider {...settings} className="px-4 md:px-8 pt-8">
           {portfolios.map(({ id, src }) => (
             <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
